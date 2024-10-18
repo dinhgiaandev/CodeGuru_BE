b0d5fa388ac1ac714aedba935d707ed013353a42
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-10-18 11:31:44 +0700

    DONE edit course - BE

--- controller/course.controller.ts
+++ controller/course.controller.ts
@@ -3,6 +3,7 @@ import CatchAsyncError from "../middleware/catchAsycnError"
 import ErrorHandler from "../utils/ErrorHandle";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
+import CourseModel from "../models/course.model";
 
 
 //upload course
@@ -26,6 +27,46 @@ export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, 
         }
 
         createCourse(data, res, next);
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+
+//edit course
+export const editCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const data = req.body;
+        const courseId = req.params.id;
+
+        //kiểm tra xem thumbnail có public_id không
+        if (data.thumbnail && data.thumbnail.public_id) {
+            await cloudinary.v2.uploader.destroy(data.thumbnail.public_id);
+        }
+
+        if (typeof data.thumbnail === 'string') {
+            const myCloud = await cloudinary.v2.uploader.upload(data.thumbnail, {
+                folder: "courses"
+            });
+
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
+            };
+        }
+
+        const course = await CourseModel.findByIdAndUpdate(
+            courseId,
+            {
+                $set: data,
+            },
+            { new: true }
+        );
+
+        res.status(201).json({
+            success: true,
+            course,
+        })
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500));
     }
--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authenticateRole, isAuthenticated } from "../middleware/auth";
-import { uploadCourse } from "../controller/course.controller";
-
-
+import { editCourse, uploadCourse } from "../controller/course.controller";
 const courseRouter = express.Router();
+
+
 courseRouter.post("/create-course", isAuthenticated, authenticateRole("admin"), uploadCourse);
+courseRouter.put("/edit-course/:id", isAuthenticated, authenticateRole("admin"), editCourse);
 
 export default courseRouter;
