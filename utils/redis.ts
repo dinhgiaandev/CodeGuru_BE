29e40973163a4e83516f711e75b992ceee3873e3
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-10-21 18:56:59 +0700

    maintance redis

--- utils/redis.ts
+++ utils/redis.ts
@@ -4,23 +4,26 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '../.env.development') });
 
+let redisClient;
+
 const connectRedis = () => {
-    if (process.env.REDIS_URL) {
-        console.log('Kết nối tới Redis...');
-        const redisClient = new Redis(process.env.REDIS_URL);
+    if (!redisClient) {
+        if (process.env.REDIS_URL) {
+            console.log('Kết nối tới Redis...');
+            redisClient = new Redis(process.env.REDIS_URL);
 
-        redisClient.on('connect', () => {
-            console.log('Redis đã kết nối thành công!');
-        });
+            redisClient.on('connect', () => {
+                console.log('Redis đã kết nối thành công!');
+            });
 
-        redisClient.on('error', (err) => {
-            console.error('Kết nối Redis thất bại:', err);
-        });
-
-        return redisClient; //trả về redis client
-    } else {
-        throw new Error('REDIS_URL không được cấu hình trong file .env');
+            redisClient.on('error', (err) => {
+                console.error('Kết nối Redis thất bại:', err);
+            });
+        } else {
+            throw new Error('REDIS_URL không được cấu hình trong file .env');
+        }
     }
+    return redisClient; //trả về redis client
 };
 
 export default connectRedis; 
